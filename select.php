e914f6b268748240bea6ca3f10e731acf2fb9a9c
g193sasa <87056060+g193sasa@users.noreply.github.com>
2021-12-15 12:56:07 +0900

    ver1.0

--- select.php
+++ select.php
@@ -7,25 +7,25 @@
     $sth1 = $pdo1 -> query($sql1);
     $aryList1 = $sth1 -> fetchAll(PDO::FETCH_COLUMN);
 
-    $sql2 = "SELECT 単語 FROM WHERE 属性 = アクション"; //name_topのアクション系の単語を取得
+    $sql2 = "SELECT 単語 FROM name_top WHERE 属性 = アクション"; //name_topのアクション系の単語を取得
     $sth2 = $pdo2 -> query($sql2);
     $aryList2 = $sth2 -> fetchAll(PDO::FETCH_COLUMN);
 
-    $sql3 = "SELECT 単語 FROM WHERE 属性 = ファンタジー"; //name_topのファンタジー系の単語を取得
+    $sql3 = "SELECT 単語 FROM name_top WHERE 属性 = ファンタジー"; //name_topのファンタジー系の単語を取得
     $sth3 = $pdo3 -> query($sql3);
     $aryList3 = $sth3 -> fetchAll(PDO::FETCH_COLUMN);
 
-    $sql4 = "SELECT 単語 FROM WHERE 属性 = コメディ"; //name_topのコメディ系の単語を取得
+    $sql4 = "SELECT 単語 FROM name_top WHERE 属性 = コメディ"; //name_topのコメディ系の単語を取得
     $sth4 = $pdo4 -> query($sql4);
     $aryList4 = $sth4 -> fetchAll(PDO::FETCH_COLUMN);
 
-    $sql5 = "SELECT 単語 FROM WHERE 属性 = 日常"; //name_topの日常系の単語を取得
+    $sql5 = "SELECT 単語 FROM name_top WHERE 属性 = 日常"; //name_topの日常系の単語を取得
     $sth5 = $pdo5 -> query($sql5);
     $aryList5 = $sth5 -> fetchAll(PDO::FETCH_COLUMN);
     
     mysql_select_db('name_bottom') or die("選択失敗"); //データベース[name_bottom]に接続
 
-    $sql6 = "SELECT 単語 FROM 'name_bottom'"; //name_bottomから単語を取得
+    $sql6 = "SELECT 単語 FROM name_bottom"; //name_bottomから単語を取得
     $sth6 = $pdo6 -> query($sql6);
     $aryList6 = $sth6 -> fetchAll(PDO::FETCH_COLUMN);
     
