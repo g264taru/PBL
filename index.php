4fc469f857332780865942909251292f32335cf4
g127kato <87055187+g127kato@users.noreply.github.com>
2021-12-20 17:32:36 +0900

    phpからjacascriptへの配列の変換ができるようになりました。

--- index.php
+++ index.php
@@ -6,68 +6,26 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    
-      <?php
+  <?php
       $con = mysql_connect('localhost','g127kato','') or die("接続失敗"); //mysqlに接続
       mysql_select_db('g127kato') or die("選択失敗"); //データベース接続
       mysql_query('SET NAMES utf8', $con);
 
       $sql1 = 'SELECT * FROM name_top'; //name_topの全属性の単語を取得
       $res = mysql_query($sql1, $con) or die("エラー");
+      $i = 0;
       while ($db = mysql_fetch_assoc($res)) {
+      $array[$i] = $db['単語'];
       echo '<table border=1 style="background-color: lavender; font-weight: bold;">';
-      echo "<tr><td>${db['id']}</td><td>${db['単語']}</td></tr></table>";
+      echo "<tr><td>$i</td><td>$array[$i]</td></tr></table>";
+      $i++;
+      $json_array = json_encode($array);//javascriptに変換するためにjson形式にする
       }
-
       mysql_close($con);
     ?>
-     /*<?php
-      $con = mysql_connect('localhost','g127kato','') or die("接続失敗"); //mysqlに接続
-      mysql_select_db('g127kato') or die("選択失敗"); //データベース接続
-      mysql_query('SET NAMES utf8', $con);
-
-      $sql1 = 'SELECT 単語 FROM name_top'; //name_topの全属性の単語を取得
-      $res = mysql_query($sql1, $con) or die("エラー");
-       while ($db = mysql_fetch_assoc($res)) {
-       echo '<table border=1 style="background-color: lavender; font-weight: bold;">';
-       echo "<tr><td>英単語</td><td>${db['単語']}</td></tr></table>";
-       }
-
-      mysql_close($con);
-    ?>*/
-    
-   /* <?php
-      $con = mysql_connect('localhost','g193sasa','') or die("接続失敗"); //mysqlに接続
-      mysql_select_db('g193sasa') or die("選択失敗"); //データベース接続
-      mysql_query('SET NAMES utf8', $con);
-
-      $sql1 = "SELECT 単語 FROM name_top"; //name_topの全属性の単語を取得
-      $sth1 = $pdo1 -> query($sql1);
-      $aryList1 = $sth1 -> fetchAll(PDO::FETCH_COLUMN);
-
-      $sql2 = "SELECT 単語 FROM name_top WHERE 属性 = アクション"; //name_topのアクション系の単語を取得
-      $sth2 = $pdo2 -> query($sql2);
-      $aryList2 = $sth2 -> fetchAll(PDO::FETCH_COLUMN);
-
-      $sql3 = "SELECT 単語 FROM name_top WHERE 属性 = ファンタジー"; //name_topのファンタジー系の単語を取得
-      $sth3 = $pdo3 -> query($sql3);
-      $aryList3 = $sth3 -> fetchAll(PDO::FETCH_COLUMN);
-
-      $sql4 = "SELECT 単語 FROM name_top WHERE 属性 = コメディ"; //name_topのコメディ系の単語を取得
-      $sth4 = $pdo4 -> query($sql4);
-      $aryList4 = $sth4 -> fetchAll(PDO::FETCH_COLUMN);
-
-      $sql5 = "SELECT 単語 FROM name_top WHERE 属性 = 日常"; //name_topの日常系の単語を取得
-      $sth5 = $pdo5 -> query($sql5);
-      $aryList5 = $sth5 -> fetchAll(PDO::FETCH_COLUMN);
-      
-      $sql6 = "SELECT 単語 FROM name_bottom"; //name_bottomから単語を取得
-      $sth6 = $pdo6 -> query($sql6);
-      $aryList6 = $sth6 -> fetchAll(PDO::FETCH_COLUMN);
-      
-      mysql_close($con);
-    ?>*/
-
+      <script type="text/javascript">
+      var foo = JSON.parse('<?php echo $json_array; ?>');//php->javascript形式で配列に保存する
+      </script>
     <div class="container">
       <ul class="menu">
         <li><a href="#" class="active" data-id="about">パスワード詳細設定</a></li>
@@ -126,7 +84,7 @@
       <input type="text" id="ans" readonly>
       <button id="generation" onClick="change_button()">生成</button>
     </div>
-    <script type="text/javascript">var foo = "<?= $host ?>";</script>
+
     <script type="text/javascript" src="js/main.js"></script>
   </body>
 </html>
